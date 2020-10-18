dfb3750c90c2b5cd382d2b322eb97256d0a14845
Artur Mostowski <artur.mostowski@student.put.poznan.pl>
2020-10-18 13:38:31 +0200

    init

---
+++ precache-manifest.e48e9da56dc5d7dbcda13c12fdb927d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3673ab215d512782b2f7639a37e2b8",
+    "url": "/rythm-manager/index.html"
+  },
+  {
+    "revision": "3a57c9839ed164342800",
+    "url": "/rythm-manager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44206acdfecea0ddaa86",
+    "url": "/rythm-manager/static/js/2.a25db31f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/rythm-manager/static/js/2.a25db31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a57c9839ed164342800",
+    "url": "/rythm-manager/static/js/main.4879aa17.chunk.js"
+  },
+  {
+    "revision": "ebb7ba75331348069382",
+    "url": "/rythm-manager/static/js/runtime-main.b223f6ea.js"
+  }
+]);
