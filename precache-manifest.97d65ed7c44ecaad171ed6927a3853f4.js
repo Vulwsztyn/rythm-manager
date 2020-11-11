71f7f912aabc0ae6502583b434f93f33b9e996ba
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2020-11-11 21:15:08 +0000

    Deploying to gh-pages from  @ a5dce0045620749b9382ead254fe693bc6369a87 🚀

---
+++ precache-manifest.97d65ed7c44ecaad171ed6927a3853f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91b2dc215b1c3ccae31304a9cfa5a26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4da30723bbbc7dff3090",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44206acdfecea0ddaa86",
+    "url": "./static/js/2.a25db31f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.a25db31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da30723bbbc7dff3090",
+    "url": "./static/js/main.eb2d6ee7.chunk.js"
+  },
+  {
+    "revision": "ebc8e5930fb4a63b509e",
+    "url": "./static/js/runtime-main.122fc0c0.js"
+  }
+]);
