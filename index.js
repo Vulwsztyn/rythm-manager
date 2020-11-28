971d700f2ed42a7f0bced90f4b617ef7ab0d281d
Artur Mostowski <artur.mostowski@currency-one.com>
2020-11-28 15:00:38 +0100

    scheduler

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const { Bot } = require('./bot')
+const { Scheduler } = require('./scheduler')
 require('dotenv').config()
 const send = require('@polka/send-type')
 const { json, urlencoded } = require('body-parser')
 const { TOKEN, VOICE_CHANNEL_ID, MSG_CHANNEL_ID, PORT } = process.env
 const polka = require('polka')
 const cors = require('cors')
+
 async function main() {
   const bot = new Bot(TOKEN, VOICE_CHANNEL_ID, MSG_CHANNEL_ID)
+  const scheduler = new Scheduler()
   await bot.init()
   polka()
     .use(cors())
@@ -17,7 +20,7 @@ async function main() {
     })
     .post('/', async (req, res) => {
       console.log(req.body.msg)
-      if (req.body.msg) await bot.send(req.body.msg)
+      if (req.body.msg) await scheduler.add(() => bot.send(req.body.msg))
       return send(res, 200, { success: true })
     })
     .listen(PORT || 3000, (err) => {
