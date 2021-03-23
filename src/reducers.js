811a6b28d4d20021e7682937070229b19ca97557
Artur Mostowski <artur.mostowski@currency-one.com>
2021-03-23 23:31:38 +0100

    Choose Prefix and Command (#1)

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,19 @@
-export const reducer = function (state = 0, action) {
+const defaultState = {
+  command: 'p',
+  prefix: '!',
+}
+export const reducer = function (state = defaultState, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
+    case 'CHANGE_COMMAND':
+      return {
+        ...state,
+        command: action.value,
+      }
+    case 'CHANGE_PREFIX':
+      return {
+        ...state,
+        prefix: action.value,
+      }
     default:
       return state
   }
