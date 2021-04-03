f28ae7ae49c9cbc685f64cf223ea6601887571c7
Artur Mostowski <artur.mostowski@currency-one.com>
2021-04-03 21:08:16 +0200

    added router

--- src/index.js
+++ src/index.js
@@ -3,27 +3,46 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { ThemeProvider } from '@material-ui/core/styles'
-import darkTheme from './theme'
 import { reducer } from './reducers'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
-        <App
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/links" children={<div>test</div>} />
+            <Route
+              path="/"
+              children={
+                <App
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                />
+              }
+            />
+            <Route
+              path=""
+              children={
+                <App
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                />
+              }
+            />
+          </Switch>
+        </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 )
 
