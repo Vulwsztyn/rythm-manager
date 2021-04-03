91b0979feeb707c6d6e1e0a1f08b75c5711c8532
Artur Mostowski <artur.mostowski@currency-one.com>
2021-04-03 23:20:22 +0200

    yt link getter

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const { Bot } = require('./bot')
-const { Scheduler } = require('./scheduler')
 require('dotenv').config()
 const send = require('@polka/send-type')
 const { json, urlencoded } = require('body-parser')
-const { TOKEN, VOICE_CHANNEL_ID, MSG_CHANNEL_ID, PORT } = process.env
 const polka = require('polka')
 const cors = require('cors')
+const {
+  TOKEN,
+  VOICE_CHANNEL_ID,
+  MSG_CHANNEL_ID,
+  PORT,
+  YT_API_KEY,
+} = process.env
+const { Bot } = require('./bot')
+const { Scheduler } = require('./scheduler')
+const { YT } = require('./yt')
+const qs = require('querystring')
 
 async function main() {
   const bot = new Bot(TOKEN, VOICE_CHANNEL_ID, MSG_CHANNEL_ID)
+  const yt = new YT(qs, YT_API_KEY)
   const scheduler = new Scheduler()
   await bot.init()
   polka()
@@ -22,6 +31,11 @@ async function main() {
       console.log(req.body.msg)
       if (req.body.msg) await scheduler.add(() => bot.send(req.body.msg))
       return send(res, 200, { success: true })
+    })
+    .post('/playlistItems', async (req, res) => {
+      const { listId } = req.body
+      const response = await yt.getList(listId)
+      return send(res, 200, response)
     })
     .listen(PORT || 3000, (err) => {
       if (err) throw err
