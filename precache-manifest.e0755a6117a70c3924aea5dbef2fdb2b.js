2b6e412035cff753783178e2d83c653d51d8b769
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2021-04-07 09:39:29 +0000

    Deploying to gh-pages from  @ bedb57a83573b4c1ac50c9ffb75e051afd7bc26f 🚀

--- precache-manifest.e0755a6117a70c3924aea5dbef2fdb2b.js
+++ precache-manifest.e0755a6117a70c3924aea5dbef2fdb2b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6621b17c2452c8c16cf2b5d6ad1bd160",
+    "revision": "2550fd981db2340ef0ca4da479689649",
     "url": "https://vulwsztyn.github.io/rythm-manager/index.html"
   },
   {
-    "revision": "57832acaba93308611e9",
+    "revision": "0dab36215abda3062f69",
     "url": "https://vulwsztyn.github.io/rythm-manager/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "795a2d09aff34ef00f11",
-    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/2.6ac9e30f.chunk.js"
+    "revision": "de7b2464ad0a11c81c67",
+    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/2.f7e6d506.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/2.6ac9e30f.chunk.js.LICENSE.txt"
+    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/2.f7e6d506.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57832acaba93308611e9",
-    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/main.713dfb8e.chunk.js"
+    "revision": "0dab36215abda3062f69",
+    "url": "https://vulwsztyn.github.io/rythm-manager/static/js/main.db659026.chunk.js"
   },
   {
     "revision": "cd5404c880a2bccb21b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://vulwsztyn.github.io/rythm-manager/precache-manifest.cf4f7011e88cd0109acb8dc7a3593e1d.js"
+  "https://vulwsztyn.github.io/rythm-manager/precache-manifest.e0755a6117a70c3924aea5dbef2fdb2b.js"
 );
 
 self.addEventListener('message', (event) => {
